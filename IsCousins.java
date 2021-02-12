3d0193457727a4de1d6e722bb90f6523b21bb5c8
MasaNakura <46548890+MasaNakura@users.noreply.github.com>
2021-02-12 15:32:12 -0800

    Update IsCousins.java

--- IsCousins.java
+++ IsCousins.java
@@ -1,35 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Exceptions - Group 3
+// Done by - Masa Nakura-Fan, Ashley Mead, Sai Murthy, Soleil Xie
 class Solution {
+    // returns true if the nodes containing values x and y have the same height but not the same parent
+    // int the given tree. returns false otherwise
     public boolean isCousins(TreeNode root, int x, int y) {
         return isCousins(root, x, y, 0) >= 0;    
     }
     
+    // returns an integer representing the height of the nodes containing the given x or y found in the given tree
+    // If the two nodes are not found or the parent of the are the same, returns -1. 
+    // @param root - given tree that is searched through
+    // @param level - the height of the current root
     private int isCousins(TreeNode root, int x, int y, int level) {
         if (root == null) return -1;
+        int left = isCousins(root.left, x, y, level + 1);
+        int right = isCousins(root.right, x, y, level + 1);
         if (root.val == x || root.val == y) {
-            int left = isCousins(root.left, x, y, level + 1); 
-            int right = isCousins(root.right, x, y, level + 1);
             if (left > 0 || right > 0) return -1;
             return level;
         }
-        int left = isCousins(root.left, x, y, level + 1);
-        int right = isCousins(root.right, x, y, level + 1);
-        System.out.println(left);
-        System.out.println(right);
         if (right == left && right > 0){
             if ((root.left.val == x && root.right.val == y) || (root.left.val == y && root.right.val == x)) {
                 return -1;
